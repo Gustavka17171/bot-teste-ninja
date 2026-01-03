798aa712cb1db5e2cad4bb62c41a619bba27d562
Gustavka17171 <mg6463785@gmail.com>
2026-01-03 00:29:42 -0300

    Atualizar o loja.js

--- commands/loja.js
+++ commands/loja.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const produtos = require("../data/produtos.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("loja")
+    .setDescription("Ver produtos"),
+  async execute(i) {
+    const e = new EmbedBuilder().setTitle("🛒 Loja").setColor("Green");
+    produtos.forEach(p =>
+      e.addFields({ name: p.nome, value: `R$${p.preco} | Estoque: ${p.estoque}` })
+    );
+    i.reply({ embeds: [e] });
+  }
+};
 
