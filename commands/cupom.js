3b5d63e058f67b7bc3b97107a6cfb4456f3c5db7
Gustavka17171 <mg6463785@gmail.com>
2026-01-03 00:28:30 -0300

    Atualizar o cupom.js

--- commands/cupom.js
+++ commands/cupom.js
@@ -1,1 +1,19 @@
+const { SlashCommandBuilder } = require("discord.js");
+const cupons = require("../data/cupons.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("cupom")
+    .setDescription("Usar cupom")
+    .addStringOption(o =>
+      o.setName("codigo").setDescription("Código do cupom").setRequired(true)
+    ),
+  async execute(i) {
+    const c = cupons.find(
+      x => x.codigo === i.options.getString("codigo")
+    );
+    if (!c) return i.reply("❌ Cupom inválido.");
+    i.reply(`🎟️ Cupom aplicado: ${c.codigo}`);
+  }
+};
 
