da6f3bcf66416e9c61efb6c5cba893509146c239
Gustavka17171 <mg6463785@gmail.com>
2026-01-03 00:28:15 -0300

    Atualizar o admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,1 +1,27 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const produtos = require("../data/produtos.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("admin")
+    .setDescription("Criar produto")
+    .addStringOption(o => o.setName("nome").setRequired(true))
+    .addIntegerOption(o => o.setName("preco").setRequired(true))
+    .addIntegerOption(o => o.setName("estoque").setRequired(true)),
+  async execute(i) {
+    if (!i.member.permissions.has("Administrator"))
+      return i.reply("❌ Sem permissão.");
+
+    produtos.push({
+      id: produtos.length + 1,
+      nome: i.options.getString("nome"),
+      preco: i.options.getInteger("preco"),
+      estoque: i.options.getInteger("estoque")
+    });
+
+    fs.writeFileSync("./data/produtos.json", JSON.stringify(produtos, null, 2));
+    i.reply("✅ Produto criado!");
+  }
+};
 
