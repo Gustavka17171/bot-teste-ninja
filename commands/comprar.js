43938c20b20556fee3ca8408bbf92995229a7c79
Gustavka17171 <mg6463785@gmail.com>
2026-01-03 00:29:22 -0300

    Atualizar o comprar.js

--- commands/comprar.js
+++ commands/comprar.js
@@ -1,1 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js");
+const carrinhos = require("../data/carrinhos.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("comprar")
+    .setDescription("Finalizar compra"),
+  async execute(i) {
+    const c = carrinhos[i.user.id];
+    if (!c || !c.length) return i.reply("🛒 Carrinho vazio.");
+
+    let total = c.reduce((s, p) => s + p.preco, 0);
+    delete carrinhos[i.user.id];
+
+    i.reply(`✅ Compra finalizada!\n💰 Total: R$${total}`);
+  }
+};
 
